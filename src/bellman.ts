acda62f6e642c4f6e7351c46f1919806262a97a4
Cry00s <lupoloic5@gmail.com>
2025-06-07 18:35:11 +0200

    UPD bellman

--- src/bellman.ts
+++ src/bellman.ts
@@ -49,15 +49,72 @@ class Bellman {
         return dist;
     }
 
-    public bellmanArborescence(source: number): number[] 
+    public bellmanArborescence(source: number): { dist: number[], parent: number[] }
     {
-        return this.bellmanCourtChemin(source);
+        const dist = new Array(this.nbSo).fill(Infinity);
+        const parent = new Array(this.nbSo).fill(-1);
+        dist[source] = 0;
+
+        for (let i = 0; i < this.nbSo - 1; i++) 
+        {
+            for (let [u, voisins] of this.arcs) 
+            {
+                for (let [v, poids] of voisins)
+                {
+                    if (dist[u] + poids < dist[v]) 
+                    {
+                        dist[v] = dist[u] + poids;
+                        parent[v] = u;
+                    }
+                }
+            }
+        }
+        return { dist, parent };
     }
 
-    public bellmanAntiArborescence(source: number): number[]
+    public inverserGraphe(): Map<number, [number, number][]> 
     {
-        return this.bellmanLongChemin(source);
+        const grapheInverse = new Map<number, [number, number][]>();
+
+        for (let i = 0; i < this.nbSo; i++) 
+        {
+            grapheInverse.set(i, []);
+        }
+
+        for (let [u, voisins] of this.arcs) 
+        {
+            for (let [v, poids] of voisins) 
+            {
+                grapheInverse.get(v)?.push([u, poids]);
+            }
+        }
+        return grapheInverse;
     }
-} 
+
+
+    public bellmanAntiArborescence(destination: number): { dist: number[], parent: number[] } 
+    {
+        const grapheInverse = this.inverserGraphe();
+        const dist = new Array(this.nbSo).fill(Infinity);
+        const parent = new Array(this.nbSo).fill(-1);
+        dist[destination] = 0;
+
+        for (let i = 0; i < this.nbSo - 1; i++) 
+        {
+            for (let [v, voisins] of grapheInverse) 
+            {
+                for (let [u, poids] of voisins) 
+                {
+                    if (dist[v] + poids < dist[u]) 
+                    {
+                        dist[u] = dist[v] + poids;
+                        parent[u] = v;
+                    }
+                }
+            }
+        }
+        return { dist, parent };
+    }
+}
 
 export { Bellman };
